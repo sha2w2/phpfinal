09152d4ee34744e364711dc407024a76ff3dc0c7
sha2w2 <164229703+sha2w2@users.noreply.github.com>
2025-05-03 00:47:55 +0300

    Added db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,25 @@
+<?
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "password_manager";
+    private $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbname);
+        
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function __destruct() {
+        $this->conn->close();
+    }
+}
+?> 
